3b19c6e67b344e17eb31558bcbfd85608c0c2b53
Antonina Kicha <antonina.kicha@gmail.com>
2023-06-04 21:59:11 +0300

    adds an avatar download

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,12 @@
 const isValidId = require("./isValidId");
 const validateBody = require("./validateBody");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
+
 
 module.exports = {
     isValidId,
     validateBody,
     authenticate,
+    upload,
 }
